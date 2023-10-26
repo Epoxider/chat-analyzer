0869997a5136960f459ca918dd79533f9b0b0224
Epoxider <matthew.r.fritz@gmail.com>
2023-10-26 11:09:53 -0700

    Big commit

--- scrape_chat.py
+++ scrape_chat.py
@@ -4,6 +4,11 @@ from twitch_listener import listener
 with open ('secrets.json', 'r') as f:
     secrets = json.load(f)
 
+with open('config.json', 'r') as f:
+    config = json.load(f)
+    channel = config['channel']
+
+
 _user_name = secrets['username']
 _oauth = secrets['oauth']
 _client_id = secrets['clientID']
@@ -13,8 +18,8 @@ bot = listener.connect_twitch(_user_name,
                              _oauth, 
                              _client_id)
 # List of channels to connect to
-channels_to_listen_to = ['hasanabi']
+channels_to_listen_to = [channel]
 # Scrape live chat data into raw log files. (Duration is seconds)
-bot.listen(channels_to_listen_to, duration = 1) 
+bot.listen(channels_to_listen_to, duration = 600) 
 # Convert log files into .CSV format
 bot.parse_logs(timestamp = True)
